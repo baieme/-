de807454535f44d571ac8af3e11b7832de73eabc
baieme <3220764367@qq.com>
2023-02-07 20:42:21 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+      <div class="div">baimen</div>
+      <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
